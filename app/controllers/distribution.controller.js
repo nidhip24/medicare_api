33cdeb005a6e0e72a6abebfeafefcd5d83bab91b
nidhip kathiriya <nidhip.kathiriya@gupshup.io>
2024-08-02 04:07:32 +0530

    bug fix

--- app/controllers/distribution.controller.js
+++ app/controllers/distribution.controller.js
@@ -8,7 +8,7 @@ exports.create = (req, res) => {
       message: "Content can not be empty!"
     });
   }
-  if (req.body.patient_birth_date.hasOwnProperty('toString')) {
+  if (req.body.patient_birth_date.hasOwnProperty('toString') || req.body.patient_birth_date == "") {
     req.body.patient_birth_date = null;
   }
 
